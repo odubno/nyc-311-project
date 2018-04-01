7bdeec34ae03d0e790ba17855c1441d92b004773
Oleh Dubno <olehdubno@gmail.com>
2018-04-01 00:37:12 -0400

    nyc311app

---
+++ nyc311app/server.R
@@ -0,0 +1,79 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(GGally)
+library(ggplot2)
+library(gridExtra)
+library(ggmap)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  df <- reactive(read.csv('data/311_filtered.csv'))
+  df_filtered_top <- reactive(read.csv('data/311_filtered_top.csv'))
+  df_borough <- reactive(read.csv('data/311_borough.csv'))
+  df_lat_lon <- reactive(read.csv("nyc311app/data/311_lat_lon.csv"))
+  
+  my_theme <- theme(plot.title = element_text(colour = "grey28", family = "Helvetica", face = "bold", size = (25)), 
+                    legend.title = element_text(colour = "midnightblue",  face = "bold.italic", family = "Helvetica", size=20), 
+                    legend.text = element_text(face = "italic", colour="mediumpurple4",family = "Helvetica", size=13), 
+                    axis.title = element_text(family = "Helvetica", size = (20), colour = "purple4"),
+                    axis.text = element_text(family = "Courier", colour = "mediumpurple2", size = (13)))
+  
+  output$boroughPlot <- renderPlot({
+    
+    p <- ggplot(df_filtered_top(), aes_string(x="Complaint.Type", y=input$borough)) +
+      labs(
+        title = "Top 15 Complaint Types", 
+        x = "Complaint Type", 
+        y = "Number of Complaints for February"
+      ) +
+      my_theme +
+      geom_bar(stat="identity") +
+      scale_y_continuous(limits=c(0,7000)) +  
+      coord_flip()
+    p
+  })
+  
+  output$boroughPlot2 <- renderPlot({
+    
+    p2 <- ggparcoord(df_borough(), columns = 2:25, groupColumn = "Borough", scale = "globalminmax")
+    p2 + labs(
+      title = "24 Hour Complaints", 
+      x = "Hours", 
+      y = "Number of Complaints"
+    ) +
+      my_theme
+  })
+  
+  output$analysis <- renderText({
+    paste(readLines("templates/analysis.html"), collapse = "\n")
+  })
+  
+  # output$summary <- renderPrint({
+  #   summary(df_filtered_top())
+  # })
+  
+  output$table <- renderTable({
+    df_filtered_top()
+  })
+  
+  output$geoPlot <- renderImage({
+    
+    list(
+      src = "images/311_heat_map.png",
+      contentType = "image/png",
+      alt = "Face"
+    )
+    
+  })
+  
+})
---
+++ nyc311app/ui.R
@@ -0,0 +1,70 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+df <- read.csv('data/311_filtered.csv')
+
+shinyUI(
+  navbarPage("What Do New Yorkers Like to Complain About",
+             
+             tabPanel(
+               "Boroughs",
+               titlePanel("Complaints By Borough"),
+               "Exploring each borough and what they like to comlain about.",
+               
+               # Generate a row with a sidebar
+               sidebarLayout(      
+                 
+                 # Define the sidebar with one input
+                 sidebarPanel(
+                   selectInput("borough", "Borough:", choices=colnames(df[-1])),
+                   hr(),
+                   helpText("Different types of complaints by borough"),
+                   hr(),
+                   h4('Code:'),
+                   a(href="https://github.com/odubno/NYC311Project/blob/master/311_bar_plot_borough.Rmd", "Bar Plot"),
+                   p(),
+                   a(href="https://github.com/odubno/NYC311Project/blob/master/311_pcp_borough.Rmd", "Parallel Plot")
+                 ),
+                 
+                 mainPanel(
+                   tabsetPanel(
+                     tabPanel("Bar Plot", plotOutput("boroughPlot")), 
+                     tabPanel("Parallel Plot", plotOutput("boroughPlot2")), 
+                     tabPanel("Geo Plot", imageOutput("geoPlot")),
+                     tabPanel("Analysis", htmlOutput("analysis")), 
+                     # tabPanel("Summary", verbatimTextOutput("summary")), 
+                     tabPanel("Table", tableOutput("table"))
+                   )
+                 )
+               )
+             ),
+             
+             tabPanel(
+               "Component 2",
+                 sidebarLayout(
+                   sidebarPanel(
+                     selectizeInput(
+                       'id', label="Year", choices=NULL, multiple=F, selected="X2015",
+                       options = list(create = TRUE,placeholder = 'Choose the year')
+                     ),
+                     # Make a list of checkboxes
+                     radioButtons("radio", label = h3("New Radio buttons"),
+                                  choices = list("Choice 1" = 1, "Choice 2" = 2)
+                     )
+                   ),
+                   mainPanel( plotOutput("distPlot") )
+                 )
+               ),
+              tabPanel("Component 3")
+  )
+)
+
+# rsconnect::deployApp('/Users/olehdubno/Documents/columbia/NYC311Project/nyc311app')
